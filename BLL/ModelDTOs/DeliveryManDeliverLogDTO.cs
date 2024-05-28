ab608a1c814c2a9cb3021cb02c75165da54f8e0a
Hujaifa81 <abuhojaifa123@gmail.com>
2024-05-28 23:07:10 +0600

    completed

---
+++ BLL/ModelDTOs/DeliveryManDeliverLogDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.ModelDTOs
+{
+    public class DeliveryManDeliveryLogDTO : DeliverymanDTO
+    {
+        public List<DeliveryLogDTO> DeliveryLogs { get; set; }
+        public DeliveryManDeliveryLogDTO()
+        {
+            DeliveryLogs = new List<DeliveryLogDTO>();
+        }
+    }
+}
---
+++ DAL/Migrations/202405150740240_Repos.cs
@@ -0,0 +1,16 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Repos : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
